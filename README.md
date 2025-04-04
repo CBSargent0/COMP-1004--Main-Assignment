3447197f18dd5e337db836817b23fc3d1f133bc8
CBSargent <90577687+CBSargent0@users.noreply.github.com>
2025-04-04 13:38:57 +0100

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# COMP-1004--Main-Assignment
-All of the files for the COMP 1004 Password Manager Assignment
---
+++ launch.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Starting Vaultaro Password Manager...
+localStorage.clear();
+start "" "login.html"
